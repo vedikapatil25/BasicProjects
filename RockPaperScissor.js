7100f0b483fa8059d5bf8f4e07cec64a42bab0c4
vedikapatil25 <vedikapatil88888@gmail.com>
2025-09-24 16:34:08 +0530

    change background color

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -12,14 +12,15 @@ showWinner = (userChoice , compChoice) => {
   else{
     if((userChoice === "rock" && compChoice === "scissor") || (userChoice === "paper" && compChoice === "rock") || (userChoice === "scissor" && compChoice === "paper")){
       msg.innerText = "You Win!!";
+      msg.style.backgroundColor = "red";
       userScore++;
       you.innerText = userScore;
     }
     else if((userChoice === "rock" && compChoice === "paper") || (userChoice === "paper" && compChoice === "scissor") || (userChoice === "scissor" && compChoice === "rock")){
       msg.innerText = "you lost";
+      msg.style.backgroundColor = "red";
       compScore++;
       comp.innerText = compScore;
-
     }
   }
 };
